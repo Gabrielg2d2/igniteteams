7fbf9074f3136c13ad75be901de01f45f27b2d2e
Gabrielg2d2 <gabriel.g2d2@gmail.com>
2023-10-02 21:35:57 -0300

    chore: config fonts expo

--- App.tsx
+++ App.tsx
@@ -2,8 +2,22 @@ import { StatusBar } from "expo-status-bar";
 import { Groups } from "./src/screens/Groups";
 import { ThemeProvider } from "styled-components/native";
 import theme from "./src/theme";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
