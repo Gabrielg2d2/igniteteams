6becb8f5e71f291649de2d5db6250e4580f8305e
Gabrielg2d2 <gabriel.g2d2@gmail.com>
2023-10-13 19:19:46 -0300

    feat: new FlatList page groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,19 +1,40 @@
+import { useState } from "react";
 import { Header } from "@components/Header";
-
-import * as S from "./styles";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
+import { FlatList } from "react-native";
+
+import * as S from "./styles";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    "Galera do teams",
+    "Galera do trampo",
+    "Galera da faculdade",
+    "Galera do nova",
+    "Galera do prédio",
+    "Galera do role",
+    "Galera do churras",
+    "Galera do futebol",
+  ]);
+
   return (
     <S.Container>
       <Header />
 
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
 
-      <GroupCard
-        title="Galera do teams"
-        onPress={() => console.log("clicou")}
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => String(item)}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+            onPress={() => console.log(`clicou: ${item}`)}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 70 }}
       />
     </S.Container>
   );
