335d22cd0de2de1912249207e392a7e483355710
Gabrielg2d2 <gabriel.g2d2@gmail.com>
2023-10-15 10:38:53 -0300

    style: placeholderTextColor

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,10 @@
 import { TextInputProps } from "react-native";
+import { useTheme } from "styled-components/native";
+
 import * as S from "./styles";
 
 export function Input(props: TextInputProps) {
-  return <S.Container {...props} />;
+  const { COLORS } = useTheme();
+
+  return <S.Container placeholderTextColor={COLORS.GRAY_300} {...props} />;
 }
