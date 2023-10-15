aacb5e31c4d2b634fa0461df0bd10322c74eacc5
Gabrielg2d2 <gabriel.g2d2@gmail.com>
2023-10-15 17:49:31 -0300

    feat: add input with icon in page Players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -3,6 +3,8 @@ import { Highlight } from "@components/Highlight";
 import * as S from "./styles";
 import { Button } from "@components/Button";
 import { ButtonIcon } from "@components/ButtonIcon";
+import { Input } from "@components/Input";
+import { Space } from "@components/Space";
 
 export type PlayersProps = {};
 
@@ -16,7 +18,12 @@ export function Players(props: PlayersProps) {
         subtitle="Adicione a galera e separe os times"
       />
 
-      <ButtonIcon icon="add" />
+      <S.Form>
+        <Input placeholder="Nome da pessoa" autoCorrect={false} />
+        <ButtonIcon icon="add" />
+      </S.Form>
+
+      <Space height={20} />
 
       <Button type="secondary" title="Remover Turma" />
     </S.Container>
