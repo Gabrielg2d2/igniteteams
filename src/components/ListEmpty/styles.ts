582bef36343596201f3ab423245be2215b2b4e07
Gabrielg2d2 <gabriel.g2d2@gmail.com>
2023-10-15 10:49:12 -0300

    style: ListEmpty

--- src/components/ListEmpty/styles.ts
+++ src/components/ListEmpty/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
@@ -8,17 +8,21 @@ export const Container = styled.View`
 
 export const Title = styled.Text`
   text-align: center;
-
-  font-size: ${({ theme }) => theme.FONT_SIZE.LG};
-  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
-  color: ${({ theme }) => theme.COLORS.GRAY_300};
   margin-bottom: 8px;
+
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.LG};
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    color: ${theme.COLORS.GRAY_300};
+  `}
 `;
 
 export const Message = styled.Text`
   text-align: center;
 
-  font-size: ${({ theme }) => theme.FONT_SIZE.LG};
-  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
-  color: ${({ theme }) => theme.COLORS.GRAY_300};
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.LG};
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    color: ${theme.COLORS.GRAY_300};
+  `}
 `;
