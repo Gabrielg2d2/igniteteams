da89dedb54113ecdd63a044e91642ac46d0b53fb
Gabrielg2d2 <gabriel.g2d2@gmail.com>
2023-10-16 19:04:44 -0300

    feat: config headerShown false

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -7,7 +7,11 @@ const { Navigator, Screen } = createNativeStackNavigator();
 
 export function AppRoutes() {
   return (
-    <Navigator>
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
       <Screen name="groups" component={Groups} />
       <Screen name="newGroups" component={NewGroup} />
       <Screen name="players" component={Players} />
