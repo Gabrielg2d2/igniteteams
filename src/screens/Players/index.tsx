3cda6c8a56e421d03c2c715ed1afba71bab77da6
Gabrielg2d2 <gabriel.g2d2@gmail.com>
2023-10-21 19:03:41 -0300

    refactor: template PlayersTemplate

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,7 +1,65 @@
+import { useRoute } from "@react-navigation/native";
+import { useNavigationCustom } from "@routes/navigationCustom";
+import { useState } from "react";
 import { PlayersTemplate, PlayersTemplateProps } from "./template";
 
+type RouteParams = {
+  group: string;
+};
+
 export function Players() {
-  const propsTemplate: PlayersTemplateProps = {};
+  const routes = useRoute();
+  const { group } = routes.params as RouteParams;
+  const { navigateToGroups } = useNavigationCustom();
+
+  const [value, setValue] = useState("");
+  const [players, setPlayers] = useState<string[]>([
+    "Jogador 1",
+    "Jogador 2",
+    "Jogador 3",
+    "Jogador 4",
+    "Jogador 5",
+    "Jogador 6",
+    "Jogador 7",
+    "Jogador 8",
+  ]);
+  const [team, setTeam] = useState("Time A");
+
+  function handleAddPlayer() {
+    if (!value) return;
+    const result = players.find(
+      (player) => player.toLowerCase() === value.toLowerCase()
+    );
+    if (result) {
+      return alert("Já existe um jogador com esse nome");
+    }
+
+    setPlayers([...players, value]);
+    setValue("");
+  }
+
+  function handleRemovePlayer(player: string) {
+    setPlayers(
+      players.filter((item) => item.toLowerCase() !== player.toLowerCase())
+    );
+  }
+
+  function handleRemoveTeam() {
+    setPlayers([]);
+  }
+
+  const propsTemplate: PlayersTemplateProps = {
+    navigateToGroups,
+    handleAddPlayer,
+    handleRemoveTeam,
+    handleRemovePlayer,
+    setTeam,
+    team,
+    setValue,
+    value,
+    players,
+    groupName: group,
+  };
 
   return <PlayersTemplate {...propsTemplate} />;
 }
--- src/screens/Players/template/index.tsx
+++ src/screens/Players/template/index.tsx
@@ -9,79 +9,51 @@ import { Space } from "@components/Space";
 import { PlayerCard } from "@components/PlayerCard";
 import { HStack } from "@components/Stacks/HStack";
 import { VStack } from "@components/Stacks/VStack";
-import { useState } from "react";
 import { FlatList } from "react-native";
 import { useTheme } from "styled-components/native";
 
 import { ListEmpty } from "@components/ListEmpty";
-import { useRoute } from "@react-navigation/native";
-import { useNavigationCustom } from "@routes/navigationCustom";
 import * as S from "./styles";
 
-type RouteParams = {
-  group: string;
+export type PlayersTemplateProps = {
+  navigateToGroups: () => void;
+  handleAddPlayer: () => void;
+  handleRemoveTeam: () => void;
+  handleRemovePlayer: (player: string) => void;
+  setTeam: (team: string) => void;
+  team: string;
+  setValue: (value: string) => void;
+  value: string;
+  players: string[];
+  groupName: string;
 };
-
-export type PlayersTemplateProps = {};
 
 export function PlayersTemplate(props: PlayersTemplateProps) {
   const { COLORS } = useTheme();
-  const routes = useRoute();
-  const { group } = routes.params as RouteParams;
-  const { navigateToGroups } = useNavigationCustom();
-
-  const [value, setValue] = useState("");
-  const [players, setPlayers] = useState<string[]>([
-    "Jogador 1",
-    "Jogador 2",
-    "Jogador 3",
-    "Jogador 4",
-    "Jogador 5",
-    "Jogador 6",
-    "Jogador 7",
-    "Jogador 8",
-  ]);
-  const [team, setTeam] = useState("Time A");
-
-  function handleAddPlayer() {
-    if (!value) return;
-    const result = players.find(
-      (player) => player.toLowerCase() === value.toLowerCase()
-    );
-    if (result) {
-      return alert("Já existe um jogador com esse nome");
-    }
-
-    setPlayers([...players, value]);
-    setValue("");
-  }
-
-  function handleRemovePlayer(player: string) {
-    setPlayers(
-      players.filter((item) => item.toLowerCase() !== player.toLowerCase())
-    );
-  }
 
   return (
     <S.Container>
-      <Header showBackButton onPressBackButton={navigateToGroups} />
+      <Header showBackButton onPressBackButton={props.navigateToGroups} />
 
       <Space space={24} />
 
-      <Highlight title={group} subtitle="Adicione a galera e separe os times" />
+      <Highlight
+        title={props.groupName}
+        subtitle="Adicione a galera e separe os times"
+      />
 
       <VStack space={12}>
         <HStack borderRadius={6} bgColor={COLORS.GRAY_700}>
           <Input
             placeholder="Nome da pessoa"
             autoCorrect={false}
-            onChange={(e) => setValue(e.nativeEvent.text)}
-            value={value}
+            onChange={(e) => props.setValue(e.nativeEvent.text)}
+            value={props.value}
           />
           <ButtonIcon
             icon="add"
-            onPress={handleAddPlayer}
-            disabled={!value.length}
+            onPress={props.handleAddPlayer}
+            disabled={!props.value.length}
           />
         </HStack>
 
@@ -92,26 +64,26 @@ export function PlayersTemplate(props: PlayersTemplateProps) {
             renderItem={({ item }) => (
               <Filter
                 title={item}
-                isActive={item.toLowerCase() === team.toLowerCase()}
-                onPress={() => setTeam(item)}
+                isActive={item.toLowerCase() === props.team.toLowerCase()}
+                onPress={() => props.setTeam(item)}
               />
             )}
             horizontal
           />
-          <S.NumberOfPlayers>{players.length}</S.NumberOfPlayers>
+          <S.NumberOfPlayers>{props.players.length}</S.NumberOfPlayers>
         </HStack>
 
         <FlatList
-          data={players}
+          data={props.players}
           keyExtractor={(item) => item}
           renderItem={({ item }) => (
             <PlayerCard
               name={item}
-              onPressRemove={() => handleRemovePlayer(item)}
+              onPressRemove={() => props.handleRemovePlayer(item)}
             />
           )}
           contentContainerStyle={
-            !players.length ? { flex: 1 } : { paddingBottom: 40 }
+            !props.players.length ? { flex: 1 } : { paddingBottom: 40 }
           }
           ListEmptyComponent={
             <ListEmpty
@@ -122,7 +94,11 @@ export function PlayersTemplate(props: PlayersTemplateProps) {
           showsVerticalScrollIndicator={false}
         />
 
-        <Button type="secondary" title="Remover Turma" />
+        <Button
+          type="secondary"
+          title="Remover Turma"
+          onPress={() => props.handleRemoveTeam()}
+        />
       </VStack>
     </S.Container>
   );
