20f56b41c03c7a9efc7941e6d72605c4ee555911
Gabrielg2d2 <gabriel.g2d2@gmail.com>
2023-10-22 09:24:12 -0300

    refactor: handleCreateGroup

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -24,8 +24,8 @@ export function NewGroup() {
 
     for (const message of messages) {
       Alert.alert("Sucesso", message);
-      const idGroup = data?.newGroup.id ?? "";
-      const namGroup = data?.newGroup.name ?? "";
+      const idGroup = data?.newGroup?.id ?? "";
+      const namGroup = data?.newGroup?.name ?? "";
       navigateToPlayers(namGroup, idGroup);
     }
   }
